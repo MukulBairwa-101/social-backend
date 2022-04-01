05cfbdd95341b95e7b35af10baec5fa25cbeed82
Mukulkumar <mukul@heliverse.com>
2022-04-01 23:55:36 +0530

    change

--- index.js
+++ index.js
@@ -3,12 +3,33 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config();
 
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req,res)=>{
+
+    const data = fs.readFileSync(
+        `${__dirname}/api/postapi.json`,"utf-8", );
+    
+    
+    
+        if(req.url == "/postapi"){
+            res.writeHead(200, {"content-type": "application/json"});
+            res.end(data);
+        }
+        else res.end('404');
+    })
+    
+
+
 // const connection = require('./db/db');
 
 // connection();
+
+
 
 app.use(express.json());
 app.use(cors());
 
 const port = process.env.PORT || 3001
-app.listen(port,()=>console.log(`server is listening at ${port}`));
+server.listen(port,()=>console.log(`server is listening at ${port}`));
